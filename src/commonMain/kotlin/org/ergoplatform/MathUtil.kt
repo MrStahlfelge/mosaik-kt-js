58d5b4a8bc5378a6ead631f89ad383199d887567
Benjamin Schulte <bs@golfgl.de>
2022-08-03 19:50:09 +0200

    Runtime work around BigDecimal bugs

--- src/commonMain/kotlin/org/ergoplatform/MathUtil.kt
+++ src/commonMain/kotlin/org/ergoplatform/MathUtil.kt
@@ -6,16 +6,17 @@ import com.ionspin.kotlin.bignum.decimal.RoundingMode
 
 
 fun BigDecimal.toLongValueWithScale(scale: Int): Long {
-    val longScale = scale.toLong()
     //val value = this.moveDecimalPoint(scale).longValue(true) <- gives wrong results, see BigNumberTest
+
+    if (!moveDecimalPoint(scale).isWholeNumber()) {
+        throw ArithmeticException()
+    }
+
+    val longScale = scale.toLong()
     val bigDecimalWhole = BigDecimal.fromBigDecimal(
         this,
         decimalMode = DecimalMode(longScale, RoundingMode.ROUND_HALF_CEILING, longScale)
     ).moveDecimalPoint(scale)
-//
-//    if (!bigDecimalWhole.isWholeNumber()) { // FIXME does not work https://github.com/ionspin/kotlin-multiplatform-bignum/issues/238
-//        throw ArithmeticException()
-//    }
 
     return bigDecimalWhole.toBigInteger() // this one is needed because longValue(true) on BigDecimal takes ages on JS
         .longValue(true)
--- src/jvmTest/kotlin/BigNumberTest.kt
+++ src/jvmTest/kotlin/BigNumberTest.kt
@@ -1,3 +1,5 @@
+import com.ionspin.kotlin.bignum.decimal.DecimalMode
+import com.ionspin.kotlin.bignum.decimal.RoundingMode
 import com.ionspin.kotlin.bignum.decimal.toBigDecimal
 import org.ergoplatform.toLongValueWithScale
 import org.ergoplatform.toPlainStringFixed
@@ -11,12 +13,31 @@ class BigNumberTest {
         assertEquals(50112345678L, bigDecimal.toLongValueWithScale(8))
         assertEquals(5011234567800L, bigDecimal.toLongValueWithScale(10))
         assertEquals(501123456780L, bigDecimal.toLongValueWithScale(9))
-        assertEquals(1123456783L, "1.1234567826".toBigDecimal().toLongValueWithScale(9)) // TODO should be an error
+        assertEquals(
+            true,
+            try {
+                "1.1234567826".toBigDecimal().toLongValueWithScale(9)
+                false
+            } catch (t: ArithmeticException) {
+                true
+            }
+        )
         assertEquals(0, "0".toBigDecimal().toLongValueWithScale(9))
     }
 
     @Test
     fun printNumberTest() {
         assertEquals("1.000000", 1000000.toBigDecimal().moveDecimalPoint(-6).scale(6).toPlainStringFixed(6))
+    }
+
+
+    // @Test 0.3.7 fixed these
+    fun testIsWhole() {
+        val bigDecimalWhole = "1.1234567826".toBigDecimal(
+            decimalMode = DecimalMode(18, RoundingMode.ROUND_HALF_CEILING, 18)
+        ).moveDecimalPoint(9)
+
+        assertEquals("1123456782.600000000000000000", bigDecimalWhole.toPlainString())
+        assertEquals(false, bigDecimalWhole.isWholeNumber())
     }
 }
